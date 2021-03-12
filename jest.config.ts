b81dfc86a284aebd4b1bd60d6b5160899993af36
dalton.leonardo <daltonleonardo@hotmail.com>
2021-03-11 22:50:02 -0300

    chore: add jest configs for mongodb

--- jest.config.ts
+++ jest.config.ts
@@ -100,6 +100,7 @@ export default {
 
   // A preset that is used as a base for Jest's configuration
   // preset: undefined,
+  preset: '@shelf/jest-mongodb',
 
   // Run tests from one or more projects
   // projects: undefined,
