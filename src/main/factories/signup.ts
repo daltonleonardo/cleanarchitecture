a8652ef9b9e88e25e2cc0a24cc64d04b0b79daa8
dalton.leonardo <daltonleonardo@hotmail.com>
2021-03-28 22:16:16 -0300

    feat: add ExpressRouteAdapter

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,8 +1,8 @@
 
 import { SignUpController } from './../../presentation/controllers/signup/signup'
-import { EmailValidatorAdapter} from '../../utils/email-validator-adapter'
-import { DbAddAccount} from '../../data/usecases/add-account'
-import { BcryptAdapter} from '../../infra/criptography/bcrypt-adapter'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 
 export const makeSignUpController = (): SignUpController => {
